589ec2ed44464f3219b69fc231194db7daced015
Alonso Huayta <alonso.hl25@gmail.com>
2018-10-02 15:30:57 -0500

    adding update for documents every day

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ var express = require('express')
 var app = express()
 var sys=require('sys')
 var cdr=require('./model/cdrcheck')
-
-
+var schedule = require('node-schedule');
+var check_sunat = require('./model/connection');
  
+var j = schedule.scheduleJob('10 * * * * *', function(){
+   
+  var currentdate = new Date(); 
+  var datetime = "Hora : " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+  console.log('============> '+ datetime);
+    check_sunat.doc_conn(function(result){//obtiene los registros que seran actualizados
+    check_sunat.doc_ins(result)//envia esos registros y los consulta con sunat
+    
+    
+  });
+});
+
+
 app.get('/', function (req, res,next) {
  
   cdr.Doc_Info("parm",function(result){
@@ -13,7 +31,17 @@ app.get('/', function (req, res,next) {
   });
 });
 
+app.get('/sunat',function(req,res,next){
+  check_sunat.doc_conn(function(result){//obtiene los registros que seran actualizados
+    check_sunat.doc_ins(result)//envia esos registros y los consulta con sunat
+    res.send(result);
+    
+  });
+});
+
 app.get('/checkdoc/:fac_ruc/:fac_tip/:fac_ser/:fac_nro', function (req, res,next) {
+
+  
 
   var json_doc =  { "fac_ruc":req.params.fac_ruc,
                     "fac_tip":req.params.fac_tip,
@@ -31,6 +59,8 @@ app.get('/checkdoc/:fac_ruc/:fac_tip/:fac_ser/:fac_nro', function (req, res,next
 app.get('/test', function (req, res,next) {
     res.send(JSON.stringify({ a: 1 ,b:10}, null, 3));
 
-  })
+  });
  
-app.listen(3000)
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
--- src/model/connection.js
+++ src/model/connection.js
@@ -1,53 +1,162 @@
 
 const sql = require('mssql')
+var cdr=require('./cdrcheck')
+//var async = require("async");
 
 const config = {
   user: 'sa',
   password: 'Panam2014',
-  server: 'apolo', // You can use 'localhost\\instance' to connect to named instance
-  database: 'docs',
+  server: 'APOLO', 
+  database: 'DB_Docs',
 
   options: {
       encrypt: false, // Use this if you're on Windows Azure
-      tdsVersion: '7_1'
+      tdsVersion: '7_4'
+     // tdsVersion: '7_1' sql 2012
   }
 }
 
+function pad_with_zeroes(number, length) {
+
+    var my_string = '' + number;
+    while (my_string.length < length) {
+        my_string = '0' + my_string;
+    }
+
+    return my_string;
+
+}
+
+async function  udp_status(var_id,status) {
+
+        await new sql.ConnectionPool(config).connect().then(pool => {
+            return pool//.input('id', sql.Int, var_id)
+            .query(`UPDATE [DB_Docs].[dbo].[Docs] SET Doc_Est =`+status.statusCode+`, Doc_StatusCode=`+status.statusCode+`, Doc_StatusMsg='`+status.statusMessage+`', Date_check=CURRENT_TIMESTAMP WHERE ID=`+var_id)
+        }).then(result => {
+            
+          //  console.log("Actualizado: "+var_id)
+            sql.close();
+        //    resolve();
+        //   callback("OK");
+        // callback(result.recordset);
+            
+        }).catch(err => {
+            //reject();
+            console.log("Error Actualizando:"+err)
+        })
+
+    //});
+
+}
+
+
+function setDelay(i) {
+    setTimeout(function(){
+        console.log("i");
+    }, 3000);
+  }
+
+
+
 
 module.exports = {
-    Doc_Info: function  Get_Doc_Info () {
+    
 
-    sql.connect(config).then(pool => {
-        // Query
-         pool.request()
-        .input('input_parameter', sql.VarChar, "F001")
-        .query('select [TipDocCod],[VenSer],[VenNroDoc],[VenFec],[VenTip] from ventas where [VenSer]=@input_parameter and [VenNroDoc]=3490')// where Dafijcod = @input_parameter')
-        })
-        .then(result => {
-            console.log(result.recordset[0].VenSer) // first recordset from result.recordsets
+    doc_conn: function  getdocdata (callback) {
+
+        new sql.ConnectionPool(config).connect().then(pool => {
+            return pool.query`SELECT TOP 50 [ID],[Doc_TipDoc],[Doc_SerDoc],[Doc_Ruc],[Doc_Nro],[Doc_Est],[Doc_FecEmi]FROM [DB_Docs].[dbo].[Docs]
+                                WHERE   date_check IS NULL and Doc_TipDoc in(01,07,08)
+                                        and ([Doc_Est] is null or [Doc_Est] = 0)
+                                        
+                                ORDER BY    date_check desc`
+        }).then(result => {
+        //    console.dir(result)
             sql.close()
-           // return result.recordsets
-            return "asd"
-            console.dir(result.recordsets)
-            //var obj = JSON.parse(result.recordsets);
-            var text = '{ "employees" : [' +
-            '{ "firstName":"John" , "lastName":"Doe" },' +
-            '{ "firstName":"Anna" , "lastName":"Smith" },' +
-            '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
-            //return JSON.parse(text);
-            //return result.recordsets;
-          //  return "test";    
-        }).catch(err => {
-                // ... error checks
-                console.dir("Error: " + err)
-            })
+            callback(result.recordset);
             
-    //res.send({ express: 'Hello From Express' });
-    return "asd"
+        }).catch(err => {
+            
+        })
+
+    },
+
+    doc_ins: function  check_data (data_docs,callback) {
+    
+        var data = [];
+
+         Promise.all(data_docs.map( (element) => {
+
+            var args_p = Promise.resolve(args = {
+                rucComprobante: element.Doc_Ruc,
+                tipoComprobante: pad_with_zeroes(element.Doc_TipDoc, 2),
+                serieComprobante: element.Doc_SerDoc,
+                numeroComprobante: element.Doc_Nro,
+            });
+
+            args_p.then(cdr.stat_check(args,udp_status,element)
+                .then(async function(res){
+
+          
+                })
+                .catch(function(e) {
+                    console.log(e); // "oh, no!"
+                    })
+            ).catch(function(e) {
+                console.log("Error Comprobando"+e); // "oh, no!"
+                });
+                
+             
+       
+
+
+      //      const contents = await fs.readFile(file, 'utf8')
+      //      console.log(contents)
+          }));
+
+/*
+        data_docs.forEach(function(element)  {
+
+
+            setDelay("i");
+/*
+  
+            var args_p = Promise.resolve(args = {
+                rucComprobante: element.Doc_Ruc,
+                tipoComprobante: pad_with_zeroes(element.Doc_TipDoc, 2),
+                serieComprobante: element.Doc_SerDoc,
+                numeroComprobante: element.Doc_Nro,
+            });
+
+            await args_p.then(cdr.stat_check(args)
+                    .then(async function(res){
+
+                        try {
+                            console.log("- 2:" );
+                            await udp_status(element.ID,res.status.statusCode,()=>{console.log("uhm")})//.then(()=>{console.log("act fin");})
+                        } catch (error) {
+                            console.log(error.stack);
+                        }
+                    })
+                    .catch(function(e) {
+                        console.log(e); // "oh, no!"
+                      })
+                );
+                
+             
+        });
+        
+       
+*/
+
+   //      });
     }
+}
+/*
+
 };
 sql.on('error', err => {
   console.dir("Error")
   // ... error handler
 })
-
+*/
